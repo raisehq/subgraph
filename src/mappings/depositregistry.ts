89a4edac753a3301039ca773e499983fc54232de
pol <pol@hero-fintech.com>
2019-10-29 17:23:26 +0100

    LP-512 added block number and timestamp for deposit kyc and funding

--- src/mappings/depositregistry.ts
+++ src/mappings/depositregistry.ts
@@ -1,47 +1,55 @@
 import {
-    UserDepositCompleted as UserDepositCompletedEvent,
-    UserWithdrawnCompleted as UserWithdrawnCompletedEvent,
-    AddressUnlockedForWithdrawal as AddressUnlockedForWithdrawalEvent
-} from "../../generated/DepositRegistry/DepositRegistry"
-import { User } from "../../generated/schema"
-import { log, BigInt } from '@graphprotocol/graph-ts'
+  UserDepositCompleted as UserDepositCompletedEvent,
+  UserWithdrawnCompleted as UserWithdrawnCompletedEvent,
+  AddressUnlockedForWithdrawal as AddressUnlockedForWithdrawalEvent
+} from "../../generated/DepositRegistry/DepositRegistry";
+import { User } from "../../generated/schema";
+import { log, BigInt } from "@graphprotocol/graph-ts";
 
-
-export function handleUserDepositCompleted(event: UserDepositCompletedEvent): void {
-    let userAddress = event.params.user;
-    let user = User.load(userAddress.toHex())
-    if (user == null) {
-        user = new User(userAddress.toHex());
-        user.address = userAddress;
-        user.referrals = [];
-        user.kyced = false;
-        user.totalBountyWithdrawn = BigInt.fromI32(0);
-        user.totalBountyToWithdraw = BigInt.fromI32(0);
-        user.totalReferralsCount = 0;
-        user.withdrawalUnlocked = false;
-        user.loanFundings = [];
-        user.loanRequests = [];
-    }
-
-    user.deposited = true;
-    
-    user.save();
-}
-
-export function handleUserWithdrawnCompleted(event: UserWithdrawnCompletedEvent): void {
-    let userAddress = event.params.user;
-    let user = new User(userAddress.toHex());
+export function handleUserDepositCompleted(
+  event: UserDepositCompletedEvent
+): void {
+  let userAddress = event.params.user;
+  let user = User.load(userAddress.toHex());
+  if (user == null) {
+    user = new User(userAddress.toHex());
+    user.address = userAddress;
+    user.referrals = [];
+    user.kyced = false;
+    user.totalBountyWithdrawn = BigInt.fromI32(0);
+    user.totalBountyToWithdraw = BigInt.fromI32(0);
+    user.totalReferralsCount = 0;
     user.withdrawalUnlocked = false;
-    user.deposited = false;
-    
-    user.save();
+    user.loanFundings = [];
+    user.loanRequests = [];
+    user.createdBlockNumber = event.block.number;
+    user.createdTimestamp = event.block.timestamp;
+  }
+
+  user.depositBlockNumber = event.block.number;
+  user.depositTimestamp = event.block.number;
+  user.deposited = true;
+
+  user.save();
 }
 
-export function handleAddressUnlockedForWithdrawal(event: AddressUnlockedForWithdrawalEvent): void {
-    let userAddress = event.params.user;
-    let user = new User(userAddress.toHex());
-    user.withdrawalUnlocked = true;
+export function handleUserWithdrawnCompleted(
+  event: UserWithdrawnCompletedEvent
+): void {
+  let userAddress = event.params.user;
+  let user = new User(userAddress.toHex());
+  user.withdrawalUnlocked = false;
+  user.deposited = false;
 
-    user.save();
+  user.save();
 }
 
+export function handleAddressUnlockedForWithdrawal(
+  event: AddressUnlockedForWithdrawalEvent
+): void {
+  let userAddress = event.params.user;
+  let user = new User(userAddress.toHex());
+  user.withdrawalUnlocked = true;
+
+  user.save();
+}
--- src/mappings/kycregistry.ts
+++ src/mappings/kycregistry.ts
@@ -1,35 +1,39 @@
 import {
-    RemovedFromKYC as RemovedFromKYCEvent,
-    AddedToKYC as AddedToKYCEvent
-} from "../../generated/KYCRegistry/KYCRegistry"
-import { User } from "../../generated/schema"
-import { log, BigInt } from '@graphprotocol/graph-ts'
+  RemovedFromKYC as RemovedFromKYCEvent,
+  AddedToKYC as AddedToKYCEvent
+} from "../../generated/KYCRegistry/KYCRegistry";
+import { User } from "../../generated/schema";
+import { log, BigInt } from "@graphprotocol/graph-ts";
 
 export function handleRemovedFromKYC(event: RemovedFromKYCEvent): void {
-    let userAddress = event.params.user;
-    let user = new User(userAddress.toHex());
-    
-    user.kyced = false;
+  let userAddress = event.params.user;
+  let user = new User(userAddress.toHex());
 
-    user.save();
+  user.kyced = false;
+
+  user.save();
 }
 
 export function handleAddedToKYC(event: AddedToKYCEvent): void {
-    let userAddress = event.params.user;
-    let user = User.load(userAddress.toHex())
-    if (user == null) {
-        user = new User(userAddress.toHex());
-        user.address = userAddress;
-        user.referrals = [];
-        user.deposited = false;
-        user.totalBountyWithdrawn = BigInt.fromI32(0);
-        user.totalBountyToWithdraw = BigInt.fromI32(0);
-        user.totalReferralsCount = 0;
-        user.loanFundings = [];
-        user.loanRequests = [];
-    }
+  let userAddress = event.params.user;
+  let user = User.load(userAddress.toHex());
+  if (user == null) {
+    user = new User(userAddress.toHex());
+    user.address = userAddress;
+    user.referrals = [];
+    user.deposited = false;
+    user.totalBountyWithdrawn = BigInt.fromI32(0);
+    user.totalBountyToWithdraw = BigInt.fromI32(0);
+    user.totalReferralsCount = 0;
+    user.loanFundings = [];
+    user.loanRequests = [];
+    user.createdBlockNumber = event.block.number;
+    user.createdTimestamp = event.block.timestamp;
+  }
 
-    user.kyced = true;
+  user.addKycBlockNumber = event.block.number;
+  user.addKycTimestamp = event.block.timestamp;
+  user.kyced = true;
 
-    user.save();
+  user.save();
 }
